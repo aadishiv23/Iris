fd56c8f954140cc488d636fae91c6a9082ff9809
Aadi Shiv Malhotra <a.malhotra@fetchrewards.com>
2025-11-02 19:17:02 -0500

    init

---
+++ Iris/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Iris
+//
+//  Created by Aadi Malhotra on 11/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Iris/IrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrisApp.swift
+//  Iris
+//
+//  Created by Aadi Malhotra on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct IrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
